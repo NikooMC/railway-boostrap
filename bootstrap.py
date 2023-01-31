07a5c29b648d226b9d47bbad249e1bb7df0b7675
SpikeBonjour <triphuongvasa@gmail.com>
2023-01-31 22:53:39 +0700

    allow restart & fix ngrok

--- bootstrap.py
+++ bootstrap.py
@@ -4,13 +4,17 @@ import engine
 from pyngrok import conf, ngrok
 
 if __name__ == "__main__":
-    subprocess.run(["curl", os.environ["SNAPSHOT"], "-o", "snapshot.tar.xz"])
-    os.mkdir("server")
+    try:
+        os.mkdir("server")
+        subprocess.run(["curl", os.environ["SNAPSHOT"], "-o", "snapshot.tar.xz"])
+    except FileExistsError:
+        pass
     engine.init(quiet=True)
     os.chdir("server")
     subprocess.run(["tar", "xf", "../snapshot.tar.xz"])
     engine.log.run_command_live_output("cat server/server.properties")
     engine.log.run_command_live_output(f"ngrok authtoken {os.environ['NGROK']}")
+    engine.log.run_command_live_output("killall ngrok")
     url = ngrok.connect(25565, 'tcp')
     print('Your server address is ' + ((str(url).split('"')[1::2])[0]).replace('tcp://', ''))
     os.chdir("..")
