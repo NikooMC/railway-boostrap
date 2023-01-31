b452be9395fce01fd9fb1003846e31cd53e8f20b
SpikeBonjour <triphuongvasa@gmail.com>
2023-01-31 12:18:20 +0700

    bruh

--- bootstrap.py
+++ bootstrap.py
@@ -5,6 +5,7 @@ import engine
 
 if __name__ == "__main__":
 	subprocess.run(["curl", os.environ["SNAPSHOT"], "-o", "snapshot.tar.xz"])
+	os.mkdir("server")
 	engine.init(quiet=True)
 	subprocess.run(["tar", "xf", "snapshot.tar.xz"])
 	engine.log.run_command_live_output("sh server/start.sh")
